1c8c90729724be2cb1f85913aa3122418b93baae
yzf <1247698516@qq.com>
2023-02-05 19:12:13 +0800

    add quicksort

--- main.go
+++ main.go
@@ -6,37 +6,40 @@ func QuickSort(nums []int, left, right int){
 	mid := PartSort(nums, left,right)
 	QuickSort(nums, left, mid - 1)
 	QuickSort(nums, mid + 1, right)
-
 }
 
 func PartSort(nums []int, left, right int)int{
-
-	key := nums[left]
-
+	mid := left
 	for left < right{
-		if left < right && nums[mi]
-
-
+		if left < right && nums[mid] >= nums[right]{
+			right--
+		}
+		if left < right && nums[mid] > nums[left]{
+			left++
+		}
+		nums[left], nums[right] = nums[right], nums[left]
 	}
-
-
+	nums[left], nums[mid] = nums[mid], nums[left]
 	return left
 }
 
 func missingNumber(nums []int) int {
-	mp := make(map[int]int)
-	for i := 0; i < len(nums) + 1;i++{
-		mp[i] = 0
-	}
-	for i := 0; i < len(nums);i++{
-		mp[nums[i]]++
-	}
-	for i, v := range mp{
-		if v == 0{
-			return i
-		}
-	}
+	QuickSort(nums, 0, len(nums) - 1)
 	return 0
+	
+	//mp := make(map[int]int)
+	//for i := 0; i < len(nums) + 1;i++{
+	//	mp[i] = 0
+	//}
+	//for i := 0; i < len(nums);i++{
+	//	mp[nums[i]]++
+	//}
+	//for i, v := range mp{
+	//	if v == 0{
+	//		return i
+	//	}
+	//}
+	//return 0
 }
 
 
